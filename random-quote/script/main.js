e63864dc369173f0c9d26bd61aad76b1a4069ad0
Stanley <Stanley@Stanleys-MacBook-Air.local>
2016-07-03 00:12:49 +0800

    responsive and color changing for random quote machine

--- random-quote/script/main.js
+++ random-quote/script/main.js
@@ -1,5 +1,7 @@
 var model = {
-  randomQuote: "Click 'New Quote' button to generate your first random quote..."
+  randomQuote: "Click 'New Quote' button to generate your first random quote...",
+  themes:["theme-0","theme-1","theme-2","theme-3","theme-4","theme-5"],
+  lastThemeNumber:0
 };
 
 var view = {
@@ -8,15 +10,31 @@ var view = {
   },
   displayNewQuote: function() {
     $("#toReplace").html(model.randomQuote);
+  },
+  changeTheme: function(className){
+    $("html").removeClass("theme-0 theme-1 theme-2 theme-3 theme-4 theme-5");
+
+    // var timeoutID = window.setTimeout(function(){
+      $("blockquote").removeClass("no-opa");
+      $("html").addClass(className);
+    // }, 500);
+
   }
 }
 
 var handler = {
   getNewQuote: function() {
-    console.log('hello,quote');
+    $("blockquote").addClass("no-opa");
     $.getJSON("http://api.adviceslip.com/advice", function(a) {
       model.randomQuote = a.slip.advice;
       view.displayNewQuote();
+      // this do while is to make sure the theme is different everytime button is clicked
+      do{
+        var num = Math.floor(Math.random() * 6);
+      }while(model.lastThemeNumber == num)
+      // to remember the current theme, to avoid the same theme next time
+      model.lastThemeNumber = num;
+      view.changeTheme(model.themes[num]);
     }); //end getJSON
   },
   tweetThis: function() {
