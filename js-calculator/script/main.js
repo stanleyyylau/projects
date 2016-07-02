9a7e396729e31419af7722b47f926d4d88b2c375
Stanley <Stanley@Stanleys-MacBook-Air.local>
2016-07-02 19:28:06 +0800

    calcualtor code clean up

--- js-calculator/script/main.js
+++ js-calculator/script/main.js
@@ -1,56 +1,51 @@
-window.onload=function(){
-	var stop=false;
-	function getValue(e){
-		if (stop) {
-			document.getElementById("valueBox").innerText="";
-			stop=!stop;
-		}
-		var valueBox=document.getElementById("valueBox");
-		var currentValue=valueBox.innerText;
-		var beingClick=e.target;
-		if(beingClick.innerText=="AC"){
-			valueBox.innerText="";
-		}else{
-		valueBox.innerText=currentValue+beingClick.innerText;
-		}
-	}//end get value function
+window.onload = function() {
+    var stop = false;
 
-	function test(){
-		if(!stop){
-		var valueBox=document.getElementById("valueBox");
-		var tobecalculated=valueBox.innerText;
-		valueBox.innerText=eval(tobecalculated);
-		stop=!stop
-		}//end if
-		}// end test function
+    function getValue(e) {
+            if (stop) {
+                document.getElementById("valueBox").innerText = "";
+                stop = !stop;
+            }
+            var valueBox = document.getElementById("valueBox");
+            var currentValue = valueBox.innerText;
+            var beingClick = e.target;
+            if (beingClick.innerText == "AC") {
+                valueBox.innerText = "";
+            } else {
+                valueBox.innerText = currentValue + beingClick.innerText;
+            }
+        } //end get value function
 
-	function backWard(){
-		var valueBox=document.getElementById("valueBox");
-		var tobedeleted=valueBox.innerText;
-		tobedeleted=tobedeleted.split("");
-		var newArray=[];
-		for (i=0;i<tobedeleted.length-1;i++){
-			newArray.push(tobedeleted[i]);
-		}
-		newArray=newArray.join("");
-		valueBox.innerText=newArray;
-		}
+    function test() {
+            if (!stop) {
+                var valueBox = document.getElementById("valueBox");
+                var tobecalculated = valueBox.innerText;
+                valueBox.innerText = eval(tobecalculated);
+                stop = !stop;
+            } //end if
+        } // end test function
 
-
-
-
-	document.getElementById("deleteOne").addEventListener("click",backWard);
-
-	document.getElementById("result").addEventListener("click",test);
+    function backWard() {
+        var valueBox = document.getElementById("valueBox");
+        var tobedeleted = valueBox.innerText;
+        tobedeleted = tobedeleted.split("");
+        var newArray = [];
+        for (i = 0; i < tobedeleted.length - 1; i++) {
+            newArray.push(tobedeleted[i]);
+        }
+        newArray = newArray.join("");
+        valueBox.innerText = newArray;
+    }
 
 
-	// Event listening for all input keys
-	var $inputs = document.querySelectorAll(".input-control");
-	for(var i=0; i < $inputs.length;i++){
-		$inputs[i].addEventListener("click",getValue);
-	}
+    // Event listening for all input keys
+    var $inputs = document.querySelectorAll(".input-control");
+    for (var i = 0; i < $inputs.length; i++) {
+        $inputs[i].addEventListener("click", getValue);
+    }
 
-
+    document.getElementById("deleteOne").addEventListener("click", backWard);
+    document.getElementById("result").addEventListener("click", test);
 
 
 }; //end window onload
